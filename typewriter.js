b411438c7e3d9fc55804233029e4e0ad85640c6e
Paul Ladd <vlendle@gmail.com>
2021-03-09 15:01:08 +0000

    Turned it into a function and exported it.

--- typewriter.js
+++ typewriter.js
@@ -1,9 +1,15 @@
-const sentence = `Tautrion Stoneglutes, Goddess-King of the Petalsworn Death-Clan, drew their deadly blade with a deadly glare, striking a deadly pose. "Watch out," they said with deadly...deadliness. "Or you'll be dead."`;
+const typewriter = string => {
+  let delay = 0;
+  for (const char of sentence) {
+    setTimeout(() => process.stdout.write(char), delay);
+    delay += 20;
+  };
 
-let delay = 0;
-for (const char of sentence) {
-  setTimeout(() => process.stdout.write(char), delay);
-  delay += 20;
+  setTimeout(() => process.stdout.write("\n"), delay);
 };
 
-setTimeout(() => process.stdout.write("\n"), delay);
+//Testing code
+//const sentence = `Tautrion Stoneglutes, Goddess-King of the Petalsworn Death-Clan, drew their deadly blade with a deadly glare, striking a deadly pose. "Watch out," they said with deadly...deadliness. "Or you'll be dead."`;
+//typewriter(sentence);
+
+module.exports = typewriter;
